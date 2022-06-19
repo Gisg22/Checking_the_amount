b520b06e9c5a6ba9bf07a0cb8c67eaaa4119cb8a
James River <gerojlordrana@gmail.com>
2022-06-19 19:39:27 +0600

    Добавьте файлы проекта.

---
+++ Checking_the_amount/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Checking_the_amount.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Checking_the_amount/Models/Sum.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Checking_the_amount.Models
+{
+    public class Sum
+    {
+        public Sum()
+        {
+            Random random = new Random();
+            ValueOne = random.Next(0, 1000);
+            ValueTwo = random.Next(0, 1000);
+            Result = ValueOne + ValueTwo;
+        }
+        public int ValueOne { get; set; }
+        public int ValueTwo { get; set; }
+        public int Result { get; set; }
+
+        [Required(ErrorMessage = "必须设置字段")]
+        [Compare("Result", ErrorMessage = "结果错误")]
+        public int InputValue { get; set; }
+    }
+}
